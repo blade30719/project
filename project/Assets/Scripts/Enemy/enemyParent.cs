b9bd8708622865c3fccd2177a7f4a37fb5592ca6
blade30719 <love470037@gmail.com>
2020-07-04 19:31:03 +0800

    02 改動: 新增資料夾分類、新增C# enemyParent

---
+++ project/Assets/Scripts/Enemy/enemyParent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class enemyParent : MonoBehaviour
+{
+ 
+	private string name;
+	private int health;
+	
+	public enemyParent(string name, int health){
+	    this.name = name;
+        this.health = health;
+	}
+    
+    public string getName(){
+        return name;
+    }
+    
+    public int getHealth(){
+        return health;
+    }
+    
+    public void setName(string name){
+        this.name = name;
+    }
+    
+    public void setHealth(int hp){
+        this.health = hp;
+    }
+	
+	abstract public bool fight();
+	abstract public bool move();
+	abstract public bool death();   
+  
+}
