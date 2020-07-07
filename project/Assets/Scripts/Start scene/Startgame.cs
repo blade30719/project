ea633d98ef609c7911e2ae3517085d19f1f96d6d
s1071486 <s1071486@gm.pu.edu.tw>
2020-07-07 15:41:57 +0800

    0707 15:41 開始到訓練關卡的切換

--- project/Assets/Scripts/Start scene/Startgame.cs
+++ project/Assets/Scripts/Start scene/Startgame.cs
@@ -1,25 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using SceneManager;
+using UnityEngine.SceneManagement;
 public class Startgame : MonoBehaviour
 {
+    //int currentScene;
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+           
+    }
+    public void startScene()//開始場景
+    {
+        SceneManager.LoadScene(0);//第一個
     }
     public void nextScene()//切換場景
     {
-       // Debug.Log("next");
-      // SceneManager.LoadScene(1);//切換到指定場景
+        SceneManager.LoadScene(1);//切換到下個
     }
+    
 }
