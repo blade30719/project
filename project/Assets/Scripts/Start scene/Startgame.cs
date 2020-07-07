f59337c06a35a3c4025bc7655fdf188ab80b4ec8
blade30719 <love470037@gmail.com>
2020-07-07 22:47:43 +0800

    0707 22:47檔案合併

--- project/Assets/Scripts/Start scene/Startgame.cs
+++ project/Assets/Scripts/Start scene/Startgame.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Startgame : MonoBehaviour
+public class startGame : MonoBehaviour
 {
     //int currentScene;
     // Start is called before the first frame update
