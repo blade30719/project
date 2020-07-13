3fd441babefff46d218f20f8d3594fd356dd752a
blade30719 <love470037@gmail.com>
2020-07-13 10:34:53 +0800

    0713 10:34 新增CatStick C#及資料夾

---
+++ project/Assets/Scripts/Enemy/cat stick/CatStick.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatStick : enemyParent
+{
+
+    public CatStick(string name , int heath) : base("catStick" , 2)
+    {
+        setName("catStick");
+        setHealth(2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D()
+    {
+
+    }
+
+    void OnTriggerExit2D()
+    {
+
+    }
+
+    protected override bool death()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool fight()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
