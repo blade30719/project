72f7f272e32d4a95bdfea81c919eb64a18080448
s1071486 <s1071486@gm.pu.edu.tw>
2020-07-15 10:16:53 +0800

    0715 10:16 新增商店連結

--- project/Assets/Scripts/Button/CatfoodStore.cs
+++ project/Assets/Scripts/Button/CatfoodStore.cs
@@ -2,26 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class startGame : MonoBehaviour
+public class CatfoodStore : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-    
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-           
+        
     }
     public void startScene()//開始場景
     {
-        SceneManager.LoadScene(0);//編號0的場景
+        SceneManager.LoadScene(3);//編號3的場景
     }
     public void nextScene()//切換場景
     {
-        SceneManager.LoadScene(1);//切換到編號1的場景
+        SceneManager.LoadScene(4);//切換到編號4的場景
     }
-    
 }
