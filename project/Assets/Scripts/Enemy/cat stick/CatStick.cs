2040ed02be36978df3ed486ce41f1563ee6fdde9
blade30719 <love470037@gmail.com>
2020-07-16 11:10:55 +0800

    0716 11:10 新增CatStickSummon 改動CatStick Scripts

--- project/Assets/Scripts/Enemy/cat stick/CatStick.cs
+++ project/Assets/Scripts/Enemy/cat stick/CatStick.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class CatStick : enemyParent
 {
 
-    float summonTime = 2.0f;
+    
     private Rigidbody2D cs;
     private Animator ani;
-    int speed = 2; //移動速度
-    [SerializeField] private GameObject catStick;
-    [SerializeField] private Transform Enemy_Position;
+    int speed ; //移動速度
     
-
     public CatStick(string name , int heath) : base("catStick" , 2)
     {
         setName("catStick");
@@ -22,7 +19,10 @@ public class CatStick : enemyParent
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", 0f, 2.0f); // 重複呼叫SpawnEnemy function()、0延遲開場就做、每summonTime秒呼叫
+        cs = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        ani.SetBool("walk",true);
+        speed = 2;
     }
 
     // Update is called once per frame
@@ -31,13 +31,7 @@ public class CatStick : enemyParent
         cs.velocity = Vector2.right * speed;
     }
 
-    void SpawnEnemy(){
-        //主堡沒爆
-        //if(){
-            Vector3 pos = new Vector3(-19,-7,-1); //宣告位置pos，Random.Range
-            Instantiate(catStick,Enemy_Position.position,Enemy_Position.rotation);
-       // }
-    }
+    
 
     void OnTriggerEnter2D()
     {
