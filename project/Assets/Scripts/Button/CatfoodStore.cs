9e24180edac6739b6d2d378c9261d5ace9950ae1
s1071486 <s1071486@gm.pu.edu.tw>
2020-07-20 22:26:07 +0800

    0720 10:20 新增商店與商品間的切換 (到時候刪除Hierarchy NewCanvas 即可)

--- project/Assets/Scripts/Button/CatfoodStore.cs
+++ project/Assets/Scripts/Button/CatfoodStore.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CatfoodStore : MonoBehaviour
 {
-    // Start is called before the first frame update
+	
+    public GameObject canvasPrefab;//要產生的canvas
+    
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
-    public void startScene()//開始場景
+    
+    public void OnClick()
     {
-        SceneManager.LoadScene(3);//編號3的場景
-    }
-    public void nextScene()//切換場景
-    {
-        SceneManager.LoadScene(4);//切換到編號4的場景
+		Debug.Log("press");
+        Instantiate (canvasPrefab, Vector2.zero, Quaternion.identity);//跑出canvasPrefab
     }
 }
