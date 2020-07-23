6f6f6899dabd26dd3e8a7091071e3827de1ca9a2
s1071486 <s1071486@gm.pu.edu.tw>
2020-07-23 11:37:08 +0800

    0723 1137

--- project/Assets/Scripts/Start scene/Startgame.cs
+++ project/Assets/Scripts/Start scene/Startgame.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class startGame : MonoBehaviour
+public class StartGame : MonoBehaviour
 {
  
     // Start is called before the first frame update
