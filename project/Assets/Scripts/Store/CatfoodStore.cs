26422bb6f4e3661a91601223840bfb29c6414af3
s1071486 <s1071486@gm.pu.edu.tw>
2020-07-24 11:29:36 +0800

    0724 11:29 商店按鈕(未完成)

---
+++ project/Assets/Scripts/Store/CatfoodStore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatfoodStore : MonoBehaviour
+{
+    public GameObject button;//要產生的button
+    void Start()
+    {
+	
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Instantiate (button, Vector2.zero, Quaternion.identity);//產生button
+    }
+    
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Destroy(GameObject.Find("Goods(Clone)"));
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+ 
+    }
+}
