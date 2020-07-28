7375703b1074151df0ca614f52bba223dd31ea01
s1071486 <s1071486@gm.pu.edu.tw>
2020-07-28 11:44:56 +0800

    0728 11:44 新增商店左右按鈕

---
+++ project/Assets/Scripts/Button/StoreRight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreRight : MonoBehaviour
+{
+    private bool IsStart = false;
+    private float LastTime = 0;
+    private bool GoRight = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GoRight == true)
+        {
+            FindObjectOfType<Cat>().MoveRight(); 
+        }
+    }
+    public void LongPress(bool bStart)//長按
+    {
+        IsStart = bStart;
+        if (IsStart)
+        {
+            LastTime = Time.time;
+            GoRight = true;
+        }
+        else if (LastTime != 0)
+        {
+            LastTime = 0;
+            GoRight = false;
+        }
+    }
+}
+
--- project/Assets/Scripts/Friendly/Cat.cs
+++ project/Assets/Scripts/Friendly/Cat.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Cat : MonoBehaviour
 {
-    //public GameObject button;//要產生的button
-    // Start is called before the first frame update
+
     void Start()
     {
         
@@ -14,21 +13,12 @@ public class Cat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            gameObject.transform.position += new Vector3(-0.1f, 0, 0);
-            
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            gameObject.transform.position += new Vector3(+0.1f,0, 0);
-        }
 
-    
-    /*void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.name == "Catfood store"){
-            Instantiate (button, Vector2.zero, Quaternion.identity);//產生button
-        }   
-    }*/
+    }
+    public void MoveLeft(){
+        gameObject.transform.position += new Vector3(-0.05f,0, 0);
+    }
+    public void MoveRight(){
+        gameObject.transform.position += new Vector3(+0.05f,0, 0);
     }
 }
