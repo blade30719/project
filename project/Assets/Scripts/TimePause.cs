fce89bdb7852125226c15956ca47857a9a5cc76f
yihan <zxac1231@gmail.com>
2020-07-29 10:43:44 +0800

    0729 10.43 timescale

---
+++ project/Assets/Scripts/TimePause.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimePause : MonoBehaviour
+{
+
+    bool continueTime = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeStop();
+        continueTime = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (continueTime == true)
+        {
+            //Debug.Log("go");
+            timeStart();
+        }
+        else
+        {
+            timeStop();
+        }
+    }
+
+    public void timeStart()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void timeStop()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void setTimeScale(bool input)
+    {
+        continueTime = input;
+    }
+}
