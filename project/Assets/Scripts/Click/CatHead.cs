cb96aed80ed16083b96dc1e182a3a234afffeb6f
s1071486 <s1071486@gm.pu.edu.tw>
2020-10-18 21:40:28 +0800

    1018 新增點擊遊戲

---
+++ project/Assets/Scripts/Click/CatHead.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CatHead : MonoBehaviour//貓頭
+{
+    Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani= GetComponent<Animator>(); //ani=所掛對象Animator
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TaskOnClick()
+    {
+        ani.SetTrigger("cathead");//開始動畫
+    }
+}
---
+++ project/Assets/Scripts/Click/Money.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Money : MonoBehaviour//錢山CatHead
+{
+    Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani= GetComponent<Animator>(); //ani=所掛對象Animator
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TaskOnClick()
+    {
+        ani.SetTrigger("money");//開始動畫
+    }
+}
