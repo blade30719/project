713bc5ff2f149a13890cbcebe0dc9c7ff6d569c4
s1071486 <s1071486@gm.pu.edu.tw>
2021-02-22 21:20:12 +0800

    2/22 21:10 新增背包未擁有灰

--- project/Assets/Scripts/養成/Furniture/F1.cs
+++ project/Assets/Scripts/養成/Furniture/F1.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 using UnityEngine.UI;
 public class F1 : MonoBehaviour
 {
+    public GameObject Pic1;
+    private Image Image1;
+
+    void Start()
+    { 
+        Image1 = Pic1.GetComponent<Image>();
+
+        if(SystemStorage.F1==0){
+            Image1.color = Color.gray;//灰
+        }else{
+            Image1.color = Color.white;//正常
+        }
+    }
     public void ClickF1()
     {
         if((SystemStorage.F1-1)<0){
             Debug.Log("庫存不足");
         }else{
             SystemStorage.F1-=1;
+            if(SystemStorage.F1==0){
+                Image1.color = Color.gray;//灰
+            }
         }   
     }
+    
 }
