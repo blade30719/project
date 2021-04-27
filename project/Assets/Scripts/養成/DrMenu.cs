eae7721dd6b30d9edacc972743515cb8ab6518e1
blade30719 <62998425+blade30719@users.noreply.github.com>
2021-04-27 22:09:29 +0800

    Revert "球clone"

--- project/Assets/Scripts/養成/DrMenu.cs
+++ project/Assets/Scripts/養成/DrMenu.cs
@@ -18,33 +18,35 @@ public class DrMenu : MonoBehaviour
     //private int value  = -1;
 
     void Start(){
-        m_Dropdown = GetComponent<Dropdown>();
-        //Debug.Log(m_Dropdown.Value);
+      //m_Dropdown = GetComponent<Dropdown>();
+      //Debug.Log(m_Dropdown.value);
     }
    
     public void InputData(int val)
     {
       if (val == 0){
-           //Debug.Log(val);
-           Instantiate (canvasPrefab1, Vector2.zero, Quaternion.identity);//產生canvasPrefab-背包家具
-           //m_Dropdown.Value = -1;
+        Debug.Log(val);
+        Instantiate (canvasPrefab1, Vector2.zero, Quaternion.identity);//產生canvasPrefab-背包家具
+        val = 4;
+        //Dropdown.Value = -1 ;
       }
       else if (val == 1){
-           //Debug.Log(val);
-           Instantiate (canvasPrefab2, Vector2.zero, Quaternion.identity);//產生canvasPrefab-背包食物
-           //m_Dropdown.Value = -1;
-        }
+        Debug.Log(val);
+        Instantiate (canvasPrefab2, Vector2.zero, Quaternion.identity);//產生canvasPrefab-背包食物
+        val = 4;
+      }
       else if (val == 2){
-          //Debug.Log(val);
-          Instantiate (canvasPrefab3, Vector2.zero, Quaternion.identity);//產生canvasPrefab-背包AA
-          //m_Dropdown.Value = -1;
-       }
+        Debug.Log(val);
+        Instantiate (canvasPrefab3, Vector2.zero, Quaternion.identity);//產生canvasPrefab-背包AA
+        val = 4;
+      }
       else if (val == 3){
-            //Debug.Log(val);
-            Instantiate (canvasPrefab4, Vector2.zero, Quaternion.identity);//產生canvasPrefab-背包BB
-            //m_Dropdown.Value = -1;
-       }
+        Debug.Log(val);
+        Instantiate (canvasPrefab4, Vector2.zero, Quaternion.identity);//產生canvasPrefab-背包BB
+        val = 4;
+      }
 
     }
+
    
 }
