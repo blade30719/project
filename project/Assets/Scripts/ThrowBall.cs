ff0e5f53c44972769bc725352f9e9820c0eda7fe
yihan221 <zxac1231@gmail.com>
2021-05-04 21:50:14 +0800

    養成丟球 0504 09.50

--- project/Assets/Scripts/ThrowBall.cs
+++ project/Assets/Scripts/ThrowBall.cs
@@ -8,9 +8,11 @@ public class ThrowBall : MonoBehaviour
     public GameObject ball;
     private float position ;
     public Rigidbody BallRb;
+    int touchcount;
 
     void Start()
     {
+        touchcount = 0;
         BallRb = GetComponent<Rigidbody>();
         position = ball.transform.position.z;
         Debug.Log(position);
@@ -20,16 +22,24 @@ public class ThrowBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        position = ball.transform.position.z;
-        if (position < 0)
+        if (Input.GetMouseButtonDown(0))
         {
-            BallRb.velocity = new Vector3(0,0,2);
-            BallRb.transform.Rotate(0,0,(float)0.5);
+            touchcount++;
         }
-        else
+        if(touchcount == 1)
         {
-            BallRb.velocity = new Vector3(0, 0, 0);
-            BallRb.useGravity = false;
+            BallRb.useGravity = true;
+            position = ball.transform.position.z;
+            if (position < 0)
+            {
+                BallRb.velocity = new Vector3(0, 0, 2);
+                //BallRb.transform.Rotate(0,0,(float)0.5);
+            }
+            else
+            {
+                BallRb.velocity = new Vector3(0, 0, 0);
+                BallRb.useGravity = false;
+            }
         }
     }
 }
