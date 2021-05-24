dadc546de64a5bad6befae5e766a18da18f447b1
yihan221 <zxac1231@gmail.com>
2021-05-24 20:55:05 +0800

    丟球

--- project/Assets/Scripts/NewThrowBall.cs
+++ project/Assets/Scripts/NewThrowBall.cs
@@ -9,7 +9,11 @@ public class NewThrowBall : MonoBehaviour
 
     public Camera follow; 
     [SerializeField] private GameObject ball;
-    private Vector2 mousePos;
+    private Vector3 MousePos;
+    private float limitX;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    int countNum = 0;
     
     
 
@@ -21,16 +25,40 @@ public class NewThrowBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //mousePos = new Vector2(Input.mousePosition.x / Screen.width * 16 , Input.mousePosition.y / Screen.height * 9);
+        //MousePos = new Vector2(Input.mousePosition.x / Screen.width * 16 , Input.mousePosition.y / Screen.height * 9);
         //transform.position = new Vector3(mousePos.x,mousePos.y,-1);
-        Vector3 mouseWorldPosition = follow.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPosition.z = -2;
-        transform.position = mouseWorldPosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            countNum++;
+        }
+
+        if (countNum == 1)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                startPos = Input.mousePosition;
+                Debug.Log(startPos);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                endPos = Input.mousePosition;
+            }
+
+            MousePos = new Vector2(Input.mousePosition.x / Screen.width - 0.5f, Input.mousePosition.y / Screen.height * 9);
+            limitX = Mathf.Clamp(transform.position.x + MousePos.x, -7.7f, 7.7f);
+            MousePos = new Vector3(limitX, transform.position.y, transform.position.z);
+            MousePos.z = -2;
+            transform.position = MousePos;
+        }
+
+
     }
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 300, 20), mousePos.ToString());
+        GUI.Label(new Rect(10, 10, 300, 20), MousePos.ToString());
         GUI.Label(new Rect(10, 30, 300, 20), ball.transform.position.ToString());
     }
 }
